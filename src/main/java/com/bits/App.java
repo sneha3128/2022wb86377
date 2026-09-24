1edd67522fedafd0915da9f100f3ded22f33cd30
Sneha T <your_github_snehatamil978@gmail.com>
2026-09-24 10:46:43 +0530

    Modified source code

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,6 +2,6 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps Assignment - 2022WB86377");
+    System.out.println("DevOps CI build triggered successfully");
     }
 }
